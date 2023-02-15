0b53dfe0ba68408c5e8efc43f29338dd53d1434d
Jackson Socolofsky <jtsoco@gmail.com>
2023-02-15 14:47:55 +0900

    fixed review typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: [:new, :create, :index, :show] do
-    member do
-      resources :reviews, only: [:create, :new]
-    end
+    resources :reviews, only: [:create, :new]
   end
 end
